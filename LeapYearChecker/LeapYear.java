0fb18be298cd7f32476bfd423d3bc214d074ca6b
Shanley Sebial <110329841+ShunMeizz@users.noreply.github.com>
2023-11-17 17:17:39 +0800

    Update LeapYear.java

--- LeapYearChecker/LeapYear.java
+++ LeapYearChecker/LeapYear.java
@@ -5,29 +5,43 @@ import FoodOrderingSystem.FoodOrder;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class LeapYear extends JFrame{
     private JTextField tfYear;
     private JButton btnCheckYear;
     private JPanel panel;
-
+    void Check(){
+        try {
+            int year = Integer.parseInt(tfYear.getText());
+            if(year==0 || year<0){
+                throw new NumberFormatException();
+            }
+            if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
+                JOptionPane.showMessageDialog(null, "Leap Year");
+            } else {
+                JOptionPane.showMessageDialog(null, "Not a leap year");
+            }
+        }catch(NumberFormatException f){
+            JOptionPane.showMessageDialog(null, "Invalid year. Try again");
+            tfYear.setText("");
+        }
+    }
     LeapYear(){
-           btnCheckYear.addActionListener(new ActionListener() {
+
+        btnCheckYear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    int year = Integer.parseInt(tfYear.getText());
-                    if(year==0 || year<0){
-                       throw new NumberFormatException();
-                    }
-                    if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
-                        JOptionPane.showMessageDialog(null, "Leap Year");
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Not a leap year");
-                    }
-                }catch(NumberFormatException f){
-                    JOptionPane.showMessageDialog(null, "Invalid year. Try again");
-                    tfYear.setText("");
+               Check();
+            }
+        });
+
+        tfYear.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode()== KeyEvent.VK_ENTER){
+                    Check();
                 }
             }
         });
